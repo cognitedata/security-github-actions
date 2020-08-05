4bbb752e293fad304c79af03650b01eb826b3910
Anders Matheson <anders.matheson@cognite.com>
2020-08-05 18:03:12 +0200

    sonarqube: Enable PR analysis

--- sonarqube/README.md
+++ sonarqube/README.md
@@ -24,10 +24,20 @@ Client ID of OAuth provider used to get the IAP token.
 
 Service account credentials for SonarQube IAP.
 
+### `pr_source_branch`
+Name of the source branch if triggered from PR.
+
+### `pr_target_branch`
+Name of the target branch if triggered from PR.
+
 ## Example usage
 
 uses: actions/sonarqube@v1
 with:
   project_path: '/path-to-project/'
   sonarqube_host: 'sonarqube.example.com'
+  sonarqube_token: ${{ secrets.SONARQUBE_TOKEN }}
   iap_client_id: '123-example.apps.googleusercontent.com'
+  iap_service_account: ${{ secrets.SONARQUBE_SERVICE_ACCOUNT }}
+  pr_source_branch: ${{ github.head_ref }}
+  pr_target_branch: ${{ github.base_ref }}
--- sonarqube/entrypoint.sh
+++ sonarqube/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -le
+#!/bin/bash -le
 # Copyright 2020 Cognite AS
 
 echo "Scanning project $GITHUB_REPOSITORY/$PROJECT_PATH"
@@ -23,9 +23,23 @@ echo ""
 
 REPO_NAME="${GITHUB_REPOSITORY##*/}"
 
+PR_ARGS=()
+if [[ "$GITHUB_REF" == refs/pull/*/merge ]] && \
+       [ -n "PR_SOURCE_BRANCH" ] && [ -n "PR_TARGET_BRANCH" ]; then
+    PR_ID="${GITHUB_REF##refs/pull/}"
+    PR_ID="${PR_ID%%/merge}"
+
+    PR_ARGS=(
+        "-Dsonar.pullrequest.branch=$PR_SOURCE_BRANCH"
+        "-Dsonar.pullrequest.key=$PR_ID"
+        "-Dsonar.pullrequest.base=$PR_TARGET_BRANCH"
+    )
+fi
+
 "$SONAR_SCANNER_HOME"/bin/sonar-scanner \
     -Dsonar.projectKey="$REPO_NAME" \
     -Dsonar.projectName="$REPO_NAME" \
     -Dsonar.sources="$PROJECT_PATH" \
     -Dsonar.host.url="http://localhost:9000" \
-    -Dsonar.login="$SONARQUBE_TOKEN"
+    -Dsonar.login="$SONARQUBE_TOKEN" \
+    "${PR_ARGS[@]}"
