f2385bb41ae99721a35b169d9b63a4042e6673bb
anders-cognite <67680649+anders-cognite@users.noreply.github.com>
2020-08-05 16:28:01 +0200

    sonarqube: Add sonarqube action (#2)

---
+++ sonarqube/README.md
@@ -0,0 +1,25 @@
+# SonarQube code scan Docker action
+
+This action runs a SonarScanner and reports to a centralized server.
+
+## Inputs
+
+### `project_path`
+
+**Optional** The path to the file or directory to scan with SonarScanner. Default `"."`.
+
+### `sonarqube_host`
+
+The hostname of the sonarqube instance.
+
+### `iap_client_id`
+
+Client ID of OAuth provider used to get the IAP token.
+
+## Example usage
+
+uses: actions/sonarqube@v1
+with:
+  project_path: '/path-to-project/'
+  sonarqube_host: 'sonarqube.example.com'
+  iap_client_id: '123-example.apps.googleusercontent.com'
---
+++ sonarqube/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -le
+# Copyright 2020 Cognite AS
+
+echo "Scanning project $GITHUB_REPOSITORY/$PROJECT_PATH"
+
+export IAP_CUSTOM_AUTH_HEADER="X-Sq-Authorization"
+python3 /iap_proxy.py &
+proxy_pid="$!"
+
+function atexit() {
+    kill "$proxy_pid"
+    echo "Waiting for proxy ($proxy_pid) to terminate"
+    wait "$proxy_pid"
+}
+trap atexit SIGINT SIGTERM EXIT
+
+echo -n "Waiting for proxy"
+until curl -s "http://localhost:9000" -o /dev/null; do
+    echo -n "."
+    sleep 1
+done
+echo ""
+
+"$SONAR_SCANNER_HOME"/bin/sonar-scanner \
+    -Dsonar.projectKey="$GITHUB_REPOSITORY" \
+    -Dsonar.projectName="$GITHUB_REPOSITORY" \
+    -Dsonar.sources="$PROJECT_PATH" \
+    -Dsonar.host.url="http://localhost:9000" \
+    -Dsonar.login="$SONARQUBE_TOKEN"
