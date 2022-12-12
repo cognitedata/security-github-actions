010b629916ee244b84db3912f2375de4868eb9c8
Bisera Milosheska <bisera.milosheska@cognite.com>
2022-12-12 09:44:40 +0100

    Update output command

--- bandit-check/entrypoint.sh
+++ bandit-check/entrypoint.sh
@@ -7,4 +7,4 @@ REPORT=$(bandit -r $1)
 REPORT="${REPORT//'%'/'%25'}"
 REPORT="${REPORT//$'\n'/'%0A'}"
 REPORT="${REPORT//$'\r'/'%0D'}"
-echo "::set-output name=report::$REPORT"
+echo "{report}={$REPORT}" >> $GITHUB_OUTPUT
